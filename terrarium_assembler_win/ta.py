e6b87a676aa570e882f083db8d415fa86f60617c
ustas <ustas@ispras.ru>
2021-07-07 22:47:47 +0300

    Implemented target option.

--- terrarium_assembler_win/ta.py
+++ terrarium_assembler_win/ta.py
@@ -604,6 +604,11 @@ msbuild  /p:OutputPath="{odir_}" /p:OutDir="{odir_}\\" /p:Configuration="{build.
                     scmd = f'7z -y x {artefact} -o{to_}'
                     lines.append(scmd)
 
+                if 'target' in it_:
+                    to_ = it_.target
+                    scmd = f'msiexec.exe /I {artefact} /QB-! INSTALLDIR="{to_}" TargetDir="{to_}"'
+                    lines.append(scmd)
+
                 if 'components' in it_:
                     msvc_components = " ".join(["--add " + comp for comp in it_.components])
 
